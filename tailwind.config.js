df4f3700119af84ba4fb71396d07809273ed0d34
akradadiya28 <akradadiya28@gmail.com>
2024-05-01 14:38:48 +0530

    Profile Card

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*"],
+  theme: {
+    fontFamily: {
+      'sans': ['"Recursive", sans-serif'],
+      'cursive': ['"Satisfy", cursive']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
